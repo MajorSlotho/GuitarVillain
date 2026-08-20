51017c9b645b3952cdb554e59450e2f3b4e7592e
MajorSlotho <Acohandy@gmail.com>
2026-08-20 15:38:44 +0200

    File order changed

--- healthbar.gd
+++ healthbar.gd
@@ -1,18 +1,18 @@
 extends CanvasGroup
 
 func _ready():
-	ChangeHP(0,load("res://HealthSymbol1.svg"))
-	ChangeHP(1,load("res://HealthSymbol1.svg"))
-	ChangeHP(2,load("res://HealthSymbol1.svg"))
+	ChangeHP(0,load("res://Sprites/HealthSymbol1.svg"))
+	ChangeHP(1,load("res://Sprites/HealthSymbol1.svg"))
+	ChangeHP(2,load("res://Sprites/HealthSymbol1.svg"))
 
 func _process(_delta):
 	match (AutoLoader.Health):
-		5: ChangeHP(0,load("res://HealthSymbol2.svg"))
-		4: ChangeHP(0,load("res://HealthSymbol3.svg"))
-		3: ChangeHP(1,load("res://HealthSymbol2.svg"))
-		2: ChangeHP(1,load("res://HealthSymbol3.svg"))
-		1: ChangeHP(2,load("res://HealthSymbol2.svg"))
-		0: ChangeHP(2,load("res://HealthSymbol3.svg"))
+		5: ChangeHP(0,load("res://Sprites/HealthSymbol2.svg"))
+		4: ChangeHP(0,load("res://Sprites/HealthSymbol3.svg"))
+		3: ChangeHP(1,load("res://Sprites/HealthSymbol2.svg"))
+		2: ChangeHP(1,load("res://Sprites/HealthSymbol3.svg"))
+		1: ChangeHP(2,load("res://Sprites/HealthSymbol2.svg"))
+		0: ChangeHP(2,load("res://Sprites/HealthSymbol3.svg"))
 
 
 func ChangeHP(NoteNum: int,NewTexture: CompressedTexture2D):
